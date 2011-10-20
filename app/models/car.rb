8b20c587e45fbf34d8b7facdd52b1197dfd26696
Jim Mitchener <jcm@packetpan.org>
2011-10-19 21:10:39 -0800

    Add validations to car form

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,10 @@
 class Car < ActiveRecord::Base
+  belongs_to :make
+
+  validates :make_id, :presence => true
+  validates :model, :presence => true
+  validates :color, :presence => true
+  validates :date_listed, :presence => true
+  validates :price, :presence => true
+  validates :year, :presence => true
 end
