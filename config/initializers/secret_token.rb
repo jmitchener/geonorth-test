117a656d5b04e816d12204cf28c6041fc4fc2b19
Jim Mitchener <jcm@packetpan.org>
2011-10-19 20:51:06 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Srdevtest::Application.config.secret_token = '7130b544669ea7b14ea9b1b68a759aa12dc7de7110e68da5352ec1a1e78253a2c36fbbf4292cd01994a7187c9d6f1a6b196499b83ee1067e7836180d439389a6'
